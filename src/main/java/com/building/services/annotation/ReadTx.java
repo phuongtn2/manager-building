fcf7c1bae3490a01e70477af51268f39825d5437
phuongtn2 <chuchuot12a15tnp@gmail.com>
2016-11-14 22:15:43 +0700

    Refs#15: add annotation

---
+++ src/main/java/com/building/services/annotation/ReadTx.java
@@ -0,0 +1,14 @@
+package com.building.services.annotation;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Transactional(rollbackFor={Throwable.class}, readOnly=true)
+public @interface ReadTx {
+}
+
